8fd9416d16c0c970a537bcc2b4991de10549fd0f
kamiokan <spherenov@gmail.com>
2022-04-28 10:26:20 +0900

    演習問題3-8 解けず

---
+++ ch3/ch3-8.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const printOut = (...str) => {
+    return str
+}
+
+console.log(printOut("魁!!男塾", "聖闘士星矢"))
+console.log(printOut("魁!!男塾"))
+
+// 「..strというrestパラメータに、文字列引数を1個渡したら、何が起きるだろうか」の動作確認
+// 特に結果を利用できるような有益な例を思い付かなかった => TODO 何かしら有益な例を見つける！
